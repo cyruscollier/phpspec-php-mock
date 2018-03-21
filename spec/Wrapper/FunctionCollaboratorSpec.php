0e22795cfdfef94052d048868e3c01214d37c2e3
Cyrus Collier <ccollier@situationinteractive.com>
2018-03-21 11:42:17 -0400

    multiple function namespaces / prophecies support

--- spec/Wrapper/FunctionCollaboratorSpec.php
+++ spec/Wrapper/FunctionCollaboratorSpec.php
@@ -3,6 +3,8 @@
 namespace spec\PhpSpec\PhpMock\Wrapper;
 
 use PhpSpec\ObjectBehavior;
+use PhpSpec\PhpMock\Prophecy\FunctionProphecy;
+use PhpSpec\PhpMock\Prophecy\FunctionProphet;
 use Prophecy\Argument;
 use PhpSpec\Locator\Resource;
 
@@ -15,9 +17,47 @@ class FunctionCollaboratorSpec extends ObjectBehavior
         $this->shouldImplement('PhpSpec\Wrapper\ObjectWrapper');
     }
     
-    function let(Resource $resource) {
-        $this->beConstructedWith($resource);
+    function let(Resource $resource, FunctionProphet $prophet) {
+        $this->beConstructedWith($resource, $prophet);
         $resource->getSrcNamespace()->willReturn('TestNamespace');
+    }
+
+    function it_gets_the_namespace()
+    {
+        $this->getNamespace()->shouldReturn('TestNamespace');
+    }
+
+    function it_sets_the_namespace()
+    {
+        $this->setNamespace('SomeOtherNamespace');
+        $this->getNamespace()->shouldReturn('SomeOtherNamespace');
+    }
+
+    function it_adds_a_function_prophecy(FunctionProphet $prophet, FunctionProphecy $prophecy)
+    {
+        $prophet->prophesize('TestNamespace')->willReturn($prophecy);
+        $this->prophesize()->shouldReturn($prophecy);
+    }
+
+    function it_adds_a_function_prophecy_with_a_new_namespace(
+        FunctionProphet $prophet,
+        FunctionProphecy $prophecy1,
+        FunctionProphecy $prophecy2
+    )
+    {
+        $prophet->prophesize('TestNamespace')->willReturn($prophecy1);
+        $this->prophesize()->shouldReturn($prophecy1);
+        $prophet->prophesize('SomeOtherNamespace')->willReturn($prophecy2);
+        $this->prophesize('SomeOtherNamespace')->shouldReturn($prophecy2);
+    }
+
+    function it_reveals_the_function_prophecy(FunctionProphet $prophet, FunctionProphecy $prophecy)
+    {
+        $this->reveal()->shouldBe(false);
+        $prophet->prophesize('TestNamespace')->willReturn($prophecy);
+        $prophecy->reveal()->shouldBeCalled();
+        $this->prophesize();
+        $this->reveal()->shouldBe(true);
     }
     
 }
--- src/Wrapper/FunctionCollaborator.php
+++ src/Wrapper/FunctionCollaborator.php
@@ -2,30 +2,34 @@
 
 namespace PhpSpec\PhpMock\Wrapper;
 
+use PhpSpec\PhpMock\Prophecy\FunctionProphet;
 use PhpSpec\Wrapper\ObjectWrapper;
-use phpmock\prophecy\PHPProphet;
 use Prophecy\Prophecy\ProphecyInterface;
 use PhpSpec\Locator\Resource;
 
 class FunctionCollaborator implements ObjectWrapper
 {
     /**
-     * @var ProphecyInterface
+     * @var ProphecyInterface[]
      */
-    protected $prophecy;
+    protected $prophecies = [];
     
     /**
-     * @var PHPProphet
+     * @var FunctionProphet
      */
     protected $function_prophet;
 
+
+    protected $namespace;
+
     /**
      * @param Resource $resource
+     * @param FunctionProphet $function_prophet
      */
-    public function __construct(Resource $resource)
+    public function __construct(Resource $resource, FunctionProphet $function_prophet = null)
     {
-        $this->function_prophet = new PHPProphet();
-        $this->prophecy = $this->function_prophet->prophesize($resource->getSrcNamespace());
+        $this->function_prophet = $function_prophet ?: new FunctionProphet();
+        $this->namespace = $resource->getSrcNamespace();
     }
 
     /**
@@ -36,29 +40,53 @@ class FunctionCollaborator implements ObjectWrapper
      */
     public function __call($method, array $arguments)
     {
-        return call_user_func_array([$this->prophecy, '__call'], [$method, $arguments]);
+        return call_user_func_array([$this->prophesize(), '__call'], [$method, $arguments]);
     }
+
+    public function getNamespace()
+    {
+        return $this->namespace;
+    }
+
+    public function setNamespace(string $namespace)
+    {
+        $this->namespace = $namespace;
+    }
+
     
     public function checkProphetPredictions()
     {
         $this->function_prophet->checkPredictions();
     }
 
-    /**
-     * @return object
-     */
     public function getWrappedObject()
     {
 
-        if (!$this->prophecy) return;
-        $object = $this->prophecy->reveal();
-        $this->prophecy = null; //so functions only get mocked once per example
-        return $object;
-        
+        if (empty($this->prophecies)) return false;
+        while(!empty($this->prophecies)) {
+            $prophecy = array_pop($this->prophecies);
+            $prophecy->reveal();
+        }
+        return true;
     }
 
     public function reveal()
     {
-        $this->getWrappedObject();
+        return $this->getWrappedObject();
+    }
+
+    /**
+     * @param string $namespace
+     * @return ProphecyInterface
+     */
+    public function prophesize(string $namespace = '')
+    {
+        if ($namespace) {
+            $this->setNamespace($namespace);
+        }
+        if (empty($this->prophecies) || $namespace) {
+            $this->prophecies[] = $this->function_prophet->prophesize($this->getNamespace());
+        }
+        return end($this->prophecies);
     }
 }
